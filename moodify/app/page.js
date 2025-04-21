dc7e66aa9b56391816ec238840e910f06799b58b
chengggkk <chengggkk@gmail.com>
2025-04-21 13:54:55 +0800

    6

--- moodify/app/page.js
+++ moodify/app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import Image from 'next/image';
 import AudioFeaturesDisplay from './components/AudioFeaturesDisplay';
 
